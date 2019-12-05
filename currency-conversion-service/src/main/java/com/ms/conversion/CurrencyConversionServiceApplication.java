4d7bcb827e521e79dfda6e0965e7e35a6d4005fe
radhakrishna-naidu-balineni <radhakrishna-naidu.balineni@hpe.com>
2019-12-05 16:14:56 +0530

    spring micro revices for feign and ribbon test app

---
+++ currency-conversion-service/src/main/java/com/ms/conversion/CurrencyConversionServiceApplication.java
@@ -0,0 +1,18 @@
+package com.ms.conversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients("com.ms.conversion")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
